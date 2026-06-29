0b6118b3e00ffaff400302d00d0284ca09b2e714
tomonari1105 <yousui1105@gmail.com>
2026-06-30 06:44:37 +0900

    First commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// ドロワーメニュー
+jQuery(".js-btn").on("click", function () {
+  jQuery(".js-menu-drawer-list").slideToggle();
+});
+
+// Topへ戻るボタン
+jQuery(".js-top-btn").on("click", function () {
+  jQuery("html, body").animate({ scrollTop: 0 }, 500);
+});
+
+jQuery(window).on("scroll", function () {
+  if (jQuery(window).scrollTop() > 80) {
+    jQuery(".js-top-btn").fadeIn(300);
+  } else {
+    jQuery(".js-top-btn").fadeOut(300);
+  }
+});
+
+// 画像をクリックすると拡大
+jQuery(".js-img-click").on("click", function () {
+  const src = jQuery(this).find("img").attr("src"); //クリックした画像（this）の src を取る
+  jQuery(".modal-img").attr("src", src); //表示用の画像に入れる
+  jQuery(".modal").addClass("is-active"); //背景にグレー半透明のカラーを目一杯入れる
+});
+
+jQuery(".modal").on("click", function () {
+  jQuery(this).removeClass("is-active"); //カラー(modal)をクリックすることでフェードアウトさせ元に戻す
+});
+
+// スムーズスクロール
+jQuery('a[href^="#"]').on("click", function (e) {
+  const speed = 300;
+  const id = jQuery(this).attr("href");
+  const target = jQuery("#" == id ? "html" : id);
+  const headerHeight = jQuery(".header").outerHeight();
+  const position = jQuery(target).offset().top - headerHeight;
+  jQuery("html,body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing",
+  );
+});
+
+jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
+  jQuery("#js-drawer-button").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
